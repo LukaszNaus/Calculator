62cdda0764112208a03240df246e31c8d25fb8ae
LukaszNaus <nauslukasz@gmail.com>
2022-09-21 18:25:05 +0200

    Added new floats

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first value");
+        float x = scanner.nextFloat()
         int x = scanner.nextInt();
         System.out.println("enter next value");
+        float y = scanner.nextFloat()
         int y = scanner.nextInt();
         System.out.println("What do you want to do?\nSelect\n1.Addition\n2.substraction\n3.Multiplication\n4.Division");
         int select = scanner.nextInt();
