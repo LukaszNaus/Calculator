89f394314d90be9151c6f421866020440b9bcca7
LukaszNaus <nauslukasz@gmail.com>
2022-09-21 18:18:14 +0200

    added int and switch case new cases

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,22 @@ public class Main {
         int x = scanner.nextInt();
         System.out.println("enter next value");
         int y = scanner.nextInt();
-        System.out.println("What do you want to do?\n1.Addition\n2.substraction\n3.Multiplication\n4.Division");
-
+        System.out.println("What do you want to do?\nSelect\n1.Addition\n2.substraction\n3.Multiplication\n4.Division");
         int select = scanner.nextInt();
+
         switch (select) {
             case 1:
-                System.out.println("");
+                System.out.println("Result " + x+y);
+                break;
+            case 2:
+                System.out.println("Result " + (x-y));
+                break;
+            case 3:
+                System.out.println("Result " + (x*y));
+                break;
+            case 4:
+                System.out.println("Result " + (x/y) );
+                break;
         }
     }
 }
